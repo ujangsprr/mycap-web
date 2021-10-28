c09d27ecde5a9313876f39ed93ff9c27d6f9422a
Ujang Supriyadi <ujangsprr@gmail.com>
2021-10-28 11:21:50 +0700

    first commit

--- client/js/client.js
+++
@@ -1,5 +0,0 @@
-console.log('Hello! I am client.js inside client/js/...');
-console.log('...This comment is on line 2 of client.js');
-
-// Insert the 'Hey there!' text on the template page
-document.getElementById('greeting').innerText = 'Hey there!';
--- server.js
+++ server.js
@@ -3,19 +3,45 @@ var path = require('path');
 
 var express = require('express');
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
 var router = express();
 var server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
 
+router.get("/", function(req, res){
+  res.sendFile(__dirname+"/client/index.html")
+})
+
+router.get("/contact", function(req, res){
+  res.sendFile(__dirname+"/client/contact.html")
+})
+
+router.get("/portofolio", function(req, res){
+  res.sendFile(__dirname+"/client/portofolio.html")
+})
+
+router.get("/tentang-kami", function(req, res){
+  res.sendFile(__dirname+"/client/tentang.html")
+})
+
+router.get("/blog", function(req, res){
+  res.sendFile(__dirname+"/client/blog.html")
+})
+
+router.get("/transkip-audio", function(req, res){
+  res.sendFile(__dirname+"/client/transkip.html")
+})
+
+router.get("/translate", function(req, res){
+  res.sendFile(__dirname+"/client/translate.html")
+})
+
+router.get("/subtitle", function(req, res){
+  res.sendFile(__dirname+"/client/subtitle.html")
+})
+
 console.log('Booting up the server! Please wait until finished...')
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+server.listen(process.env.PORT || 5000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("All ready! Server listening at", addr.address + ":" + addr.port);
 });
